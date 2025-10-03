c53f2f4c246172ca5afef6035d16b08a410aadcf
Prapti <praptiyush2007@gmail.com>
2025-10-04 01:59:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,66 +1,127 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
 from datetime import datetime
+import os
 
+# -------------------- App Setup --------------------
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db= SQLAlchemy(app)
-class Todo(db.Model):
-    sno=db.Column(db.Integer, primary_key=True)
-    title=db.Column(db.String(200), primary_key=False)
-    desc=db.Column(db.String(500), primary_key=False)
-    date_created= db.Column(db.DateTime, default=datetime.utcnow)
-    def __repr__(self) -> str:
-        return f"{self.sno}- {self.title}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "supersecretkey"  # Change in production
 
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# -------------------- Models --------------------
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    todos = db.relationship('Todo', backref='owner', lazy=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo {self.title}>"
+
+# -------------------- Flask-Login Setup --------------------
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# -------------------- Routes --------------------
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if request.method=='POST':
-        title= request.form['title']
-        desc= request.form['desc']
-        todo= Todo(title=title, desc=desc)
+@login_required
+def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc, user_id=current_user.id)
         db.session.add(todo)
         db.session.commit()
-    allTodo= Todo.query.all()
-    return render_template('index.html', allTodo= allTodo)
+        flash("Todo added successfully!", "success")
+    allTodo = Todo.query.filter_by(user_id=current_user.id).all()
+    return render_template('index.html', allTodo=allTodo, user=current_user)
 
-@app.route('/show')
-def show():
-    allTodo= Todo.query.all()
-    print(allTodo)
-    return "this is products page"
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Username already exists!", "danger")
+            return redirect(url_for('register'))
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        flash("Registration successful! Please log in.", "success")
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash("Login successful!", "success")
+            # Directly redirect to main todo page
+            return redirect(url_for('home'))
+        else:
+            flash("Invalid credentials!", "danger")
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.", "info")
+    return redirect(url_for('login'))
 
 @app.route('/delete/<int:sno>')
+@login_required
 def delete(sno):
-    todo= Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
-    db.session.commit()
+    todo = Todo.query.filter_by(sno=sno, user_id=current_user.id).first()
+    if todo:
+        db.session.delete(todo)
+        db.session.commit()
+        flash("Todo deleted successfully!", "info")
     return redirect("/")
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
+@login_required
 def update(sno):
-    if request.method=='POST':
-        title= request.form['title']
-        desc= request.form['desc']
-        todo= Todo.query.filter_by(sno=sno).first()
-        todo.title=title
-        todo.desc=desc
-        db.session.add(todo)
+    todo = Todo.query.filter_by(sno=sno, user_id=current_user.id).first()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
         db.session.commit()
+        flash("Todo updated successfully!", "success")
         return redirect("/")
-    
-    todo= Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html', todo= todo)
+    return render_template('update.html', todo=todo)
 
-
-@app.route('/test')
-def test():
-    return "Test page working!"
-
+# -------------------- Run App --------------------
 if __name__ == "__main__":
+    # Ensure database exists and tables are created
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
